33716f8ee79480935a7384c975f48b192fd9c6e9
Akash-Pardasani <smartavi004@gmail.com>
2016-10-18 00:42:18 +0530

    Update pali_scrap.py

--- pali_scrap.py
+++ pali_scrap.py
@@ -1,3 +1,5 @@
+#using bleach and BeautifulSoup for scraping from GRETIL
+
 import urllib2
 from bs4 import BeautifulSoup
 import os
@@ -8,46 +10,8 @@ def make_dir(directory):
 	if not os.path.exists(directory):
 			os.makedirs(directory) 
 
-url_list = [["file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/2_parcan/milind1u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/2_parcan/milind2u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/2_parcan/milindou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/2_parcan/milindpu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/2_parcan/nettip_u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/2_parcan/petako_u.htm"],
-			["file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/3_chron/dhatuvau.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/3_chron/dipav_2u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/3_chron/dipava1u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/3_chron/dipava2u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/3_chron/dipavmsu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/3_chron/hattha_u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/3_chron/mahava_u.htm"],
-			["file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/buvismou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/buvismpu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_1ou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_1pu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_2ou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_2pu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_3ou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_3pu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_4ou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_4pu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_5ou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_5pu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_6ou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_6pu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_7ou.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/samp_7pu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/4_comm/visudd_u.htm"],
-			 
-			["file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/6_suanco/lokapp_u.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/6_suanco/upasak_u.htm"],
-			 
-			["file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/9_phil/dhkariku.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/9_phil/mkacbheu.htm",
-			 "file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/9_phil/yaskacsu.htm"],
-			["file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/9_phil/lex/abhidh_u.htm"],
-			["file:///C:/Users/ADMIN/Desktop/Pali_GRETEL/2_pali/9_phil/rhet/subodh_u.htm"]]
-																													
+url_list = []
+#working on it																													
 
 foldername = ["2_parcan","3_chron","4_comm","6_suanco","9_phil","9_phil/lex","9_phil/rhet"]
 
@@ -71,8 +35,4 @@ for j in (1,7):
 		target = open(path_,'w')
 		clean = bleach.clean(h, tags = [], strip = True)
 		target.write(clean.encode('utf8'))
-	k = k + 1
-	
-	
-	
-raw_input("Press:")	
+	k = k + 1	
